0e9afea58800dfe792f42509edaa2cde452ec9ef
Paul Pedro <unkerpaulie@gmail.com>
2024-05-14 19:09:25 -0400

    created the authentication app and basic pages

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class RegisterView(View):
+    def get(self, req):
+        return render(req, "authentication/register.html")
+
+        
+class LoginView(View):
+    def get(self, req):
+        return render(req, "authentication/login.html")
+
