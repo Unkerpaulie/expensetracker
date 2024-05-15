23c22aa18a0dd35e5719a0081fba105f4d1eecef
Paul Pedro <unkerpaulie@gmail.com>
2024-05-15 10:42:43 -0400

    added checking feedback in registration for slow response

--- core/static/js/register.js
+++ core/static/js/register.js
@@ -1,6 +1,7 @@
 // username validation
 const usernameField = document.getElementById("username");
 const usernameFeedback = document.getElementById("username_feedback");
+const usernameCheck = document.getElementById("username_check");
 
 usernameField.addEventListener("keyup", (e) => {
     const usernameEntry = e.target.value;
@@ -8,6 +9,9 @@ usernameField.addEventListener("keyup", (e) => {
     usernameField.classList.remove("is-invalid");
     usernameFeedback.style.display = "none";
     usernameFeedback.innerText = "";
+    // show checking message while waiting for fetch response
+    usernameCheck.style.display = "block";
+    usernameCheck.textContent = `Checking...${usernameEntry}`;
 
     if (usernameEntry.length > 0) {
         fetch("/auth/username_val", {
@@ -16,6 +20,7 @@ usernameField.addEventListener("keyup", (e) => {
         })
         .then((res) => res.json())
         .then((data => {
+            usernameCheck.style.display = "none";
             if (data.username_error) {
                 usernameField.classList.add("is-invalid");
                 usernameFeedback.style.display = "block";
@@ -28,6 +33,7 @@ usernameField.addEventListener("keyup", (e) => {
 // email validation
 const emailField = document.getElementById("email");
 const emailFeedback = document.getElementById("email_feedback");
+const emailCheck = document.getElementById("email_check");
 
 emailField.addEventListener("keyup", (e) => {
     const emailEntry = e.target.value;
@@ -35,6 +41,9 @@ emailField.addEventListener("keyup", (e) => {
     emailField.classList.remove("is-invalid");
     emailFeedback.style.display = "none";
     emailFeedback.innerText = "";
+    // show checking message while waiting for fetch response
+    emailCheck.style.display = "block";
+    emailCheck.textContent = `Checking...${emailEntry}`;
 
     if (emailEntry.length > 0) {
         fetch("/auth/email_val", {
@@ -43,6 +52,7 @@ emailField.addEventListener("keyup", (e) => {
         })
         .then((res) => res.json())
         .then((data => {
+            emailCheck.style.display = "none";
             if (data.email_error) {
                 emailField.classList.add("is-invalid");
                 emailFeedback.style.display = "block";
@@ -56,6 +66,7 @@ emailField.addEventListener("keyup", (e) => {
 const passwordToggle = document.getElementById("passwordToggle");
 const passwordField = document.getElementById("password");
 const passwordField2 = document.getElementById("password2");
+const password2Check = document.getElementById("password2_check");
 
 passwordToggle.addEventListener("click", (e) => {
     if (passwordToggle.textContent == "SHOW") {
@@ -71,6 +82,7 @@ passwordToggle.addEventListener("click", (e) => {
 
 // confirm passsword match
 const passwordFeedback2 = document.getElementById("password2_feedback");
+
 passwordField2.addEventListener("keyup", (e) => {
     passwordField2.classList.remove("is-invalid");
     passwordFeedback2.style.display = "none";
