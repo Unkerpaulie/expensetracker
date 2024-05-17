1f12920f5bb41848263f4635b5bb2861ff440e24
Paul Pedro <unkerpaulie@gmail.com>
2024-05-17 12:13:52 -0400

    added log in and log out functionality

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, LoginView, UsernameValidationView, EmailValidationView
+from .views import RegisterView, LoginView, LogoutView, UsernameValidationView, EmailValidationView
 from django.views.decorators.csrf import csrf_exempt
 
 app_name = "auth"
@@ -9,4 +9,5 @@ urlpatterns = [
     path("username_val", csrf_exempt(UsernameValidationView.as_view()), name="username_val"),
     path("email_val", csrf_exempt(EmailValidationView.as_view()), name="email_val"),
     path("login", LoginView.as_view(), name="login"),
+    path("logout", LogoutView.as_view(), name="logout"),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.models import User
-from django.contrib import messages
+from django.contrib import messages, auth
 import json, re
 from django.http import JsonResponse
+from django.core.mail import EmailMessage
 
 
 def validate_email(email):
     regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
     return re.match(regex, email) is not None
 
+def send_activation_email(user):
+    body = """
+Please follow the link provide to activate your account.
+"""
+    email = EmailMessage(
+        subject="Activate you Expense Tracke account",
+        body=body,
+        from_email="noreply@localhost.run",
+        to=user.email,
+    )
 
 class RegisterView(View):
     def get(self, req):
@@ -26,7 +37,9 @@ class RegisterView(View):
         user = User(username=username, email=email)
         user.set_password(password)
         user.save()
-        
+
+        # set up cnfirmation email to activate accout
+
         if user.id:
             messages.success(req, "Registration successful! You may now log in")
             return redirect("auth:login")
@@ -66,3 +79,27 @@ class LoginView(View):
         context = {"page": "login"}
         return render(req, "authentication/login.html", context)
 
+    def post(self, req):
+        # info already valid
+        data = req.POST
+        username = data["username"]
+        password = data["password"]
+
+        user = auth.authenticate(username=username, password=password)
+        # set up cnfirmation email to activate accout
+
+        if user:
+            auth.login(req, user)
+            messages.success(req, f"Welcome, {user.username}!")
+            return redirect("core:home")
+
+        # if an error occured, return user to form with values restored
+        messages.warning(req, "Incorrect credentials. Please try again.")
+        context = {"page": "login", "fieldvalues": data}
+        return render(req, "authentication/login.html", context)
+
+
+class LogoutView(View):
+    def get(self, req):
+        auth.logout(req)
+        return redirect("core:home")
