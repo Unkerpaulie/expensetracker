2604a51845f48ba4e57ab9aed3d44ff0211c8fc6
Paul Pedro <unkerpaulie@gmail.com>
2024-06-23 07:27:37 -0400

    added total to dashboard

--- core/static/js/dash.js
+++ core/static/js/dash.js
@@ -29,6 +29,21 @@ function showchart(data, ctx) {
   });
 }
 
+function updatetotals(data, m){
+  let disptotal = data["inc_chart"].total - data["exp_chart"].total;
+  const monthNames = ["Overall", "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+  
+  if (disptotal > 0) {
+    totaltitle.innerHTML = `<span class="text-success"><svg class="bi"><use xlink:href="#graph-up-arrow"/></svg> ${monthNames[m]} Surplus</span>`;
+  } else if (disptotal < 0) {
+    totaltitle.innerHTML = `<span class="text-danger"><svg class="bi"><use xlink:href="#graph-down-arrow"/></svg> ${monthNames[m]} Deficit</span>`;
+  } else {
+    totaltitle.innerHTML = `<span class="text-primary"><svg class="bi"><use xlink:href="#magic"/></svg> ${monthNames[m]} is Balanced</span>`;
+  }
+  totaldisplay.innerHTML = `${disptotal} ${data["inc_chart"].currency}`;
+}
 
 async function buildchart(endpoint, chart_cont, key, m=0) {
   // destroy and remake canvas
@@ -55,6 +70,7 @@ async function buildchart(endpoint, chart_cont, key, m=0) {
     cv.classList.add("w-100");
     chart_cont.appendChild(cv);
     await showchart(data[key], cv);
+    updatetotals(data, m);
   } else {
     chart_cont.innerHTML = "<h4 class=\"mt-4 text-muted\">No data found</h4>"
   }
@@ -66,9 +82,12 @@ const mnth = document.getElementById("month");
 const exp_cont = document.getElementById("exp_cont");
 const inc_cont = document.getElementById("inc_cont");
 
+const totaldisplay = document.getElementById("totaldisplay");
+const totaltitle = document.getElementById("totaltitle");
+
+
 buildchart("month_charts/", exp_cont, "exp_chart");
 buildchart("month_charts/", inc_cont, "inc_chart");
-
 mnthBtn.addEventListener("click", () => {
   const m = mnth.value;
   buildchart("month_charts/", exp_cont, "exp_chart", m);
